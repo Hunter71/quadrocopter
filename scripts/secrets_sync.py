48dd6bd04f644c7db7792843c3386c5e502a8203
Tomasz Zięba <tomasz.zieba.71@gmail.com>
2025-04-22 22:10:31 +0200

    check values

--- scripts/secrets_sync.py
+++ scripts/secrets_sync.py
@@ -53,7 +53,7 @@ def sync_secrets(public_key_id, public_key):
         secret_name = f"{secret['name']}_{ENVIRONMENT.upper()}"
         if secret_value := os.getenv(secret["name"]):
             print(secret_name, secret_value)
-            assert secret_value == "foo-app-id"
+            assert secret_value in [f"{ENVIRONMENT}-app-id", f"{ENVIRONMENT}-app-secret"]
         # encrypted_value = encrypt_secret(public_key, secret_value)
         # create_or_update_repo_secret(secret_name, encrypted_value, public_key_id)
 
