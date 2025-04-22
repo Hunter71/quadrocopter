672e67b68d440742ce233606a30974dbb5005dba
Tomasz Zięba <tomasz.zieba.71@gmail.com>
2025-04-22 21:54:53 +0200

    update

--- scripts/secrets_sync.py
+++ scripts/secrets_sync.py
@@ -1,17 +1,20 @@
-import requests
-import os
 import base64
-# import nacl.encoding
-# import nacl.public
+import os
+
+import requests
+import nacl.encoding
+import nacl.public
 
 # Set your GitHub token and repository details
 GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
+ENVIRONMENT = os.getenv('ENVIRONMENT')
 
 # GitHub API URLs
 BASE_URL = f'https://api.github.com/repos/Hunter71/quadrocopter'
 ENV_SECRETS_URL = f'{BASE_URL}/environments'
 REPO_SECRETS_URL = f'{BASE_URL}/actions/secrets'
 PUBLIC_KEY_URL = f'{BASE_URL}/actions/secrets/public-key'
+
 
 # Headers for authentication
 headers = {
@@ -25,11 +28,11 @@ def get_environment_secrets(environment):
     response.raise_for_status()
     return response.json().get('secrets', [])
 
-# def encrypt_secret(public_key: str, secret_value: str) -> str:
-#     public_key = nacl.public.PublicKey(public_key.encode("utf-8"), nacl.encoding.Base64Encoder())
-#     sealed_box = nacl.public.SealedBox(public_key)
-#     encrypted = sealed_box.encrypt(secret_value.encode("utf-8"))
-#     return base64.b64encode(encrypted).decode("utf-8")
+def encrypt_secret(public_key: str, secret_value: str) -> str:
+    public_key = nacl.public.PublicKey(public_key.encode("utf-8"), nacl.encoding.Base64Encoder())
+    sealed_box = nacl.public.SealedBox(public_key)
+    encrypted = sealed_box.encrypt(secret_value.encode("utf-8"))
+    return base64.b64encode(encrypted).decode("utf-8")
 
 def create_or_update_repo_secret(secret_name, encrypted_value, public_key_id):
     url = f'{REPO_SECRETS_URL}/{secret_name}'
@@ -46,16 +49,13 @@ def get_public_key():
     return response.json()
 
 def sync_secrets(public_key_id, public_key):
-    environments = ['foo', 'bar']  # Add your environments here
-    for env in environments:
-        secrets = get_environment_secrets(env)
-        for secret in secrets:
-            secret_name = f"{secret['name']}_{env.upper()}"
-            secret_value = os.getenv(secret["name"])
-            # secret_value = secret['value']  # Assuming you have access to the plain value
-            print(secret_value)
-            # encrypted_value = encrypt_secret(public_key, secret_value)
-            # create_or_update_repo_secret(secret_name, encrypted_value, public_key_id)
+    for secret in get_environment_secrets(ENVIRONMENT):
+        secret_name = f"{secret['name']}_{ENVIRONMENT.upper()}"
+        if secret_value := os.getenv(secret["name"]):
+            print(secret_name, secret_value)
+            assert secret_value == "foo-app-id"
+        # encrypted_value = encrypt_secret(public_key, secret_value)
+        # create_or_update_repo_secret(secret_name, encrypted_value, public_key_id)
 
 if __name__ == '__main__':
     public_key_info = get_public_key()
