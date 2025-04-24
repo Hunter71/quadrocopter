83d7d1e30330ce48db4c65840c462a16f421a5db
Tomasz Zięba <tomasz.zieba.71@gmail.com>
2025-04-24 16:59:20 +0200

    updates

--- scripts/secrets_sync.py
+++ scripts/secrets_sync.py
@@ -2,61 +2,62 @@ import base64
 import os
 
 import requests
-import nacl.encoding
-import nacl.public
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
 
-# Set your GitHub token and repository details
-GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
-ENVIRONMENT = os.getenv('ENVIRONMENT')
+class GitHubSecretManager:
+    def __init__(self, github_token, environment):
+        self.github_token = github_token
+        self.environment = environment
+        self.base_url = f'https://api.github.com/repos/Hunter71/quadrocopter'
+        self.env_secrets_url = f'{self.base_url}/environments'
+        self.repo_secrets_url = f'{self.base_url}/actions/secrets'
+        self.public_key_url = f'{self.base_url}/actions/secrets/public-key'
+        self.headers = {
+            'Authorization': f'token {self.github_token}',
+            'Accept': 'application/vnd.github.v3+json'
+        }
 
-# GitHub API URLs
-BASE_URL = f'https://api.github.com/repos/Hunter71/quadrocopter'
-ENV_SECRETS_URL = f'{BASE_URL}/environments'
-REPO_SECRETS_URL = f'{BASE_URL}/actions/secrets'
-PUBLIC_KEY_URL = f'{BASE_URL}/actions/secrets/public-key'
+    def get_environment_secrets(self):
+        url = f'{self.env_secrets_url}/{self.environment}/secrets'
+        response = requests.get(url, headers=self.headers)
+        response.raise_for_status()
+        return response.json().get('secrets', [])
 
+    def encrypt_secret(self, public_key: str, secret_value: str) -> str:
+        public_key = RSA.import_key(public_key.encode('utf-8'))
+        cipher = PKCS1_OAEP.new(public_key)
+        encrypted = cipher.encrypt(secret_value.encode('utf-8'))
+        return base64.b64encode(encrypted).decode('utf-8')
 
-# Headers for authentication
-headers = {
-    'Authorization': f'token {GITHUB_TOKEN}',
-    'Accept': 'application/vnd.github.v3+json'
-}
+    def create_or_update_repo_secret(self, secret_name, encrypted_value, public_key_id):
+        url = f'{self.repo_secrets_url}/{secret_name}'
+        data = {
+            'encrypted_value': encrypted_value,
+            'key_id': public_key_id,
+        }
+        response = requests.put(url, headers=self.headers, json=data)
+        response.raise_for_status()
 
-def get_environment_secrets(environment):
-    url = f'{ENV_SECRETS_URL}/{environment}/secrets'
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()
-    return response.json().get('secrets', [])
+    def get_public_key(self):
+        response = requests.get(self.public_key_url, headers=self.headers)
+        response.raise_for_status()
+        return response.json()
 
-def encrypt_secret(public_key: str, secret_value: str) -> str:
-    public_key = nacl.public.PublicKey(public_key.encode("utf-8"), nacl.encoding.Base64Encoder())
-    sealed_box = nacl.public.SealedBox(public_key)
-    encrypted = sealed_box.encrypt(secret_value.encode("utf-8"))
-    return base64.b64encode(encrypted).decode("utf-8")
-
-def create_or_update_repo_secret(secret_name, encrypted_value, public_key_id):
-    url = f'{REPO_SECRETS_URL}/{secret_name}'
-    data = {
-        'encrypted_value': encrypted_value,
-        'key_id': public_key_id,
-    }
-    response = requests.put(url, headers=headers, json=data)
-    response.raise_for_status()
-
-def get_public_key():
-    response = requests.get(PUBLIC_KEY_URL, headers=headers)
-    response.raise_for_status()
-    return response.json()
-
-def sync_secrets(public_key_id, public_key):
-    for secret in get_environment_secrets(ENVIRONMENT):
-        secret_name = f"{secret['name']}_{ENVIRONMENT.upper()}"
-        if secret_value := os.getenv(secret["name"]):
-            print(secret_name, secret_value)
-            assert secret_value == os.getenv(secret_name)
-            # encrypted_value = encrypt_secret(public_key, secret_value)
-            # create_or_update_repo_secret(secret_name, encrypted_value, public_key_id)
+    def sync_secrets(self):
+        public_key_info = self.get_public_key()
+        public_key_id = public_key_info['key_id']
+        public_key = public_key_info['key']
+        for secret in self.get_environment_secrets():
+            secret_name = f"{secret['name']}_{self.environment.upper()}"
+            if secret_value := os.getenv(secret["name"]):
+                # encrypted_value = self.encrypt_secret(public_key, secret_value)
+                # self.create_or_update_repo_secret(secret_name, encrypted_value, public_key_id)
+                print(secret_name, secret_value)
+                assert secret_value == f"{self.environment}-{secret['name'].lower().replace("_", "-")}"
 
 if __name__ == '__main__':
-    public_key_info = get_public_key()
-    sync_secrets(public_key_info['key_id'], public_key_info['key'])
+    github_token = os.getenv('GITHUB_TOKEN')
+    environment = os.getenv('ENVIRONMENT')
+    syncer = GitHubSecretManager(github_token, environment)
+    syncer.sync_secrets()
